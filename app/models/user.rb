7899535e7ae9dbc3516686fb8ed2b9d4a589c7bc
Akash Joshi <akashjoshirm.aj@gmail.com>
2025-03-03 09:53:58 +0530

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   has_many :articles
-
+  has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_27_125626) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_03_041910) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_27_125626) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
